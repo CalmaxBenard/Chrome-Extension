f92f1b9afa72d2ba62a04d8628a26d017c3e9809
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-27 03:23:22 +0300

    create leads array add event listener to save btn

---
+++ script.js
@@ -0,0 +1,8 @@
+let myLeads = []
+
+const inputBtn = document.querySelector("#input-btn")
+const inputEl = document.querySelector("#input-el")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from an event listener")
+})
