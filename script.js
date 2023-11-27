30f59c691bdd8fe878010f42bf25718797502ee2
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-27 13:16:40 +0300

    persist chrome refresh

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 let myLeads = []
 
+// myLeads = JSON.parse(myLeads)
+// myLeads.push("www.epiclead.com")
+// console.log(myLeads)
+
+// myLeads = JSON.stringify(myLeads)
+// console.log(myLeads)
+
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 
+// localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+// console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     clearFunction()
+    
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    // let lead = JSON.parse(localStorage.getItem("myLeads"))
+    // leadsFromLocalStorage.push(lead)
+    
+    
     renderLeads()
+    // console.log(localStorage.getItem("myLeads"))
+
+    
 })
 
 function renderLeads() {
