17dd66c8688867a11700589383e15b00bd657d81
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-27 04:00:03 +0300

    display the leads in ul

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
 })
 
+for (let i = 0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
