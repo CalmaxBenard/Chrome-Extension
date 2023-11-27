f03e79765dd730cd3e98bafa67c171dc087e6586
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-27 03:58:51 +0300

    get user input values

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from an event listener")
+    myLeads.push(inputEl.value)
 })
+
