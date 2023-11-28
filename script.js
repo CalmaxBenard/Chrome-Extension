fe42b7d34ae7e51282ccc3cf88d433ca194e0dd3
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-28 09:54:45 +0300

    add save tab function

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 
 // retrieve leads from localStorage
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
@@ -21,6 +22,20 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+// const tabs = [
+//     {
+//         url: "https://www.linkedin.com/in/calmax-benard/"
+//     }
+// ]
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ""
